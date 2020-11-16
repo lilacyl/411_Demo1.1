90b73cb82077f8ce4b6320ffa516356279a11e42
sywang <42976623+wangsongyuan98@users.noreply.github.com>
2020-11-15 21:00:22 -0600

    database created

--- project/app/models.py
+++ project/app/models.py
@@ -7,11 +7,26 @@ class Test(models.Model):
     def __str__(self):
         return self.name
 
-
 class StockInfo(models.Model):
     stock_id = models.IntegerField(default=0)
     company_name = models.CharField(max_length=50)
     growth_rate = models.IntegerField(default=0)
 
     def __str__(self):
-        return '%s %s %s' % (self.stock_id, self.company_name, self.growth_rate)
+        return self.company_name
+
+class FinancialProduct(models.Model):
+    fp_id = models.IntegerField(default=0)
+
+class StructuredFinancialInvestment(models.Model):
+    fp_id = models.ForeignKey(FinancialProduct, on_delete=models.CASCADE, verbose_name="the financial product id")
+    stock_id = models.ForeignKey(StockInfo, on_delete=models.CASCADE, verbose_name="the stock id")
+    SFI_id = models.IntegerField(default=0)
+    Knock_in = models.IntegerField(default=0)
+    Knock_out = models.IntegerField(default=0)
+    put_strike = models.IntegerField(default=0)
+
+
+
+
+
